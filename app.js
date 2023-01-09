aa2160b1b30a7b27324acc300c974f975b8807d2
Jake Hersalia <jhersalia@securitybank.com.ph>
2023-01-09 18:14:04 +0800

    Fixed error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var statuses = require("./__dummyData__/statuses.json");
+var tasks = require("./__dummyData__/tasks.json");
 
 app.get("/statuses", (req, res, next) => {
   res.json(statuses);
